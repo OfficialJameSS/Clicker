9e2f4a393cf61a627e681666f388a27449aad34c
JacobeeQ <73647980+JacobeeQ@users.noreply.github.com>
2020-10-29 13:34:54 +0100

    Clicker (game)
    30 seconds! Click and get points. Can you get 150, 200 or 300 points? Play this game!

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 69);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wynik = 0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(24, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CLICK!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(329, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Czas: 0s";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.button2.Location = new System.Drawing.Point(0, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(54, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "?";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 272);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Clicker (speed test)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int i { get; private set; }
+        public int czas { get; private set; }
+        public int dzieleniesredniej { get; private set; }
+        public double srednia { get; private set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            i = 0;
+            dzieleniesredniej = 30;
+            czas = 30;
+            timer1.Start();
+            label1.Text = "Wynik = " + Convert.ToString(i);
+            label2.Text = "Czas: " + Convert.ToString(czas) + "s";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            czas--;
+            label2.Text = "Czas: " + czas + "s";
+
+            if (czas <= 0)
+            {
+                timer1.Stop();
+                srednia = Convert.ToDouble(i) / Convert.ToDouble(dzieleniesredniej);
+                DialogResult dialogResult = MessageBox.Show("Twój czas się skończył. Oto statystyki!\nWynik = " + i + "\nŚrednia = " + srednia , "Czy chcesz ponowić próbę?" , MessageBoxButtons.YesNoCancel);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    i = 0;
+                    czas = 30;
+                    timer1.Start();
+                    label1.Text = "Wynik = " + Convert.ToString(i);
+                    label2.Text = "Czas: " + Convert.ToString(czas);
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i++;
+            label1.Text = "Wynik = " + Convert.ToString(i);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Masz 30 sekund, aby sprawdzić swoją szybkość klikania. Każde kliknięcie jest liczone za 1 punkt.");
+        }
+    }
+}
